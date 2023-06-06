1e20595de315f03410e4c56bdcdfa76271804ca4
jojohimawan <jordan.hiphop21@gmail.com>
2023-06-06 20:07:29 +0700

    chore: dashboard routing and styling

--- dashboard/pages/manajemen_transaksi/index.php
+++ dashboard/pages/manajemen_transaksi/index.php
@@ -29,7 +29,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
-    <title>Home</title>
+    <link rel="icon"  href="./../../../assets/favicon.jpg" />
+    <title>Dashboard - Manajemen Transaksi</title>
 </head>
 <body class="bg-slate-50">
 
@@ -44,6 +45,12 @@
     <div class="h-full px-3 py-8 overflow-y-auto bg-white border-r">
         <ul class="font-medium">
             <li>
+                <a href="./../../../" class="flex items-center justify-around p-5 text-slate-700 text-2xl font-semibold rounded-lg bg-white mb-10">
+                <img src="./../../../assets/Logo.jpg" class="w-6" alt="Logo">
+                <span class="">Ajoor</span>
+                </a>
+            </li>
+            <li>
                 <a href="./../../index.php" class="flex items-center p-5 text-slate-500 rounded-lg hover:bg-slate-100 mb-2">
                 <svg aria-hidden="true" class="w-6 h-6 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
                 <span class="ml-3">Listing Produk</span>
@@ -51,16 +58,17 @@
             </li>
             <li>
                 <a href="./../transaksi" class="flex items-center p-5 text-slate-500 rounded-lg hover:bg-slate-100 mb-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-gray-500 transition duration-75">
-                    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z" clip-rule="evenodd" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path d="M19.5 21a3 3 0 003-3v-4.5a3 3 0 00-3-3h-15a3 3 0 00-3 3V18a3 3 0 003 3h15zM1.5 10.146V6a3 3 0 013-3h5.379a2.25 2.25 0 011.59.659l2.122 2.121c.14.141.331.22.53.22H19.5a3 3 0 013 3v1.146A4.483 4.483 0 0019.5 9h-15a4.483 4.483 0 00-3 1.146z" />
                 </svg>
                 <span class="ml-3">Transaksi</span>
                 </a>
             </li>
             <li>
                 <a href="#" class="flex items-center p-5 text-white rounded-lg hover:bg-green-700 bg-green-600 mb-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6 text-gray-500 transition duration-75">
-                    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z" clip-rule="evenodd" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
+                    <path d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 01-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004zM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 01-.921.42z" />
+                    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v.816a3.836 3.836 0 00-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 01-.921-.421l-.879-.66a.75.75 0 00-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 001.5 0v-.81a4.124 4.124 0 001.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 00-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 00.933-1.175l-.415-.33a3.836 3.836 0 00-1.719-.755V6z" clip-rule="evenodd" />
                 </svg>
                 <span class="ml-3">Manajemen Transaksi</span>
                 </a>
@@ -172,7 +180,7 @@
                             </td>
                             <td class="px-6 py-4 font-medium text-base text-slate-500">
                                 <?php if($tran["total_harga"] !== '0') : ?>
-                                    <img src="./../../../public/img/<?= $tran['bukti_bayar'] ?>" alt="Image">
+                                    <img src="./../../../public/img/<?= $tran['bukti_bayar'] ?>" class="w-40" alt="Image">
                                 <?php else : ?>
                                     -
                                 <?php endif; ?>
@@ -183,9 +191,10 @@
                             </td>
                             <td class="px-6 py-4 font-medium text-base text-slate-500">
                                 <?php if($tran["status"] === 'pending') : ?>
-                                    <a href="./approval/index.php?transaksi_id=<?= $tran["transaksi_id"] ?>" type="button" class="flex gap-x-2 items-center justify-center focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Setujui Transaksi <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
-                                        <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
-                                        </svg>
+                                    <a href="./approval/index.php?transaksi_id=<?= $tran["transaksi_id"] ?>" type="button" class="flex gap-x-2 items-center justify-center focus:outline-none text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Setujui Transaksi <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                                        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+                                    </svg>
+
                                     </a>
                                 <?php else : ?>
                                     -
--- dashboard/pages/update_listing/index.php
+++ dashboard/pages/update_listing/index.php
@@ -44,7 +44,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
-    <title>Home</title>
+    <link rel="icon"  href="./../../../assets/favicon.jpg" />
+    <title>Update Produk</title>
 </head>
 <body class="bg-slate-50">
 
