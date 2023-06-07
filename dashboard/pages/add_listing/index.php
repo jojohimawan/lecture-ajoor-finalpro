eee61aca3bd647f5783afff321e423bd93203053
jojohimawan <jordan.hiphop21@gmail.com>
2023-06-07 12:07:40 +0700

    chore: form display

--- dashboard/pages/add_listing/index.php
+++ dashboard/pages/add_listing/index.php
@@ -189,7 +189,7 @@
                 <textarea id="deskripsi" name="deskripsi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-[10em] p-2.5" required></textarea>
             </div>
             <div class="mb-6">
-                <label for="harga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga</label> <!-- harga -->
+                <label for="harga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga(isi 0 untuk freebie)</label> <!-- harga -->
                 <input type="number" id="harga" name="harga" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
             </div>
             <div class="mb-6">
--- dashboard/pages/update_listing/index.php
+++ dashboard/pages/update_listing/index.php
@@ -127,7 +127,7 @@
                 <textarea id="deskripsi" name="deskripsi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-[10em] p-2.5"><?= $row['deskripsi'] ?></textarea>
             </div>
             <div class="mb-6">
-                <label for="harga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga</label> <!-- harga -->
+                <label for="harga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga (isi 0 untuk freebie)</label> <!-- harga -->
                 <input type="number" id="harga" name="harga" value="<?= $row['harga'] ?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
             </div>
             <div class="mb-6">
